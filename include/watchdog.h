0292d9495647dc109614d7d20f9bf00d7e1e84ee
Francois Gervais <francoisgervais@gmail.com>
2024-06-04 12:31:51 -0400

    Add watchdog and reset

--- include/watchdog.h
+++ include/watchdog.h
@@ -1,7 +1,7 @@
-#ifndef INIT_H_
-#define INIT_H_
+#ifndef WATCHDOG_H_
+#define WATCHDOG_H_
 
 int init_watchdog(const struct device *wdt,
 		  int *main_channel_id, int *mqtt_channel_id);
 
-#endif /* INIT_H_ */
+#endif /* WATCHDOG_H_ */
